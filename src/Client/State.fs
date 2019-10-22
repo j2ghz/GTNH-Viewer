bc67d6c6295e1ef737d86b528639d0878f0940cb
J2ghz <j2.00ghz@gmail.com>
2019-10-22 13:15:26 +0200

    add navbar

--- src/Client/State.fs
+++ src/Client/State.fs
@@ -7,36 +7,43 @@ open Elmish.Navigation
 
 let pageHash =
     function
-    | Types.Page.Home -> "#"
+    | Types.Page.Home -> "#/Home"
     | Types.Page.Quests _ -> "#/Quests"
     | Types.Page.Recipes -> "#/Recipes"
 
+let urlUpdate (result: Page option) model: AppModel * Cmd<AppMsg> =
+    match result with
+    | Some p -> { model with CurrentPage = Some p }, Cmd.Empty
+    | None -> (model, Cmd.ofMsg <| NavigateTo Home)
 
 let init (route: Page option): AppModel * Cmd<AppMsg> =
     let quests, questsCmds =
         (match route with
-         | Some (Quests p) -> Some p
+         | Some(Quests p) -> Some p
          | _ -> None)
         |> Quests.State.init
-    { CurrentPage = route
-      Quests = quests }, Cmd.batch [ Cmd.map QuestsMsg questsCmds ]
+
+    let model, cmd =
+        (urlUpdate route
+             { CurrentPage = route
+               Quests = quests })
+
+    model,
+    Cmd.batch
+        [ Cmd.map QuestsMsg questsCmds
+          cmd ]
 
 let route state =
     oneOf
-        [ map Home (s "Home" )
+        [ map Home (s "Home")
           map Recipes (s "Recipes")
           map (Quests Quests.Types.Page.Home) (s "Quests") ] state
 
 let a loc = parseHash route loc
-
-let urlUpdate (result: Option<Page>) model: AppModel * Cmd<AppMsg> =
-    match result with
-    | Some p -> { model with CurrentPage = Some p }, Cmd.Empty
-    | None -> (model, Navigation.modifyUrl "#")
 
 let update (msg: AppMsg) (state: AppModel): AppModel * Cmd<AppMsg> =
     match msg with
     | QuestsMsg qmsg ->
         let questsState, questsCmd = Quests.State.update state.Quests qmsg
         { state with Quests = questsState }, Cmd.map QuestsMsg questsCmd
-    | NavigateTo r -> failwith "needed?"
+    | NavigateTo r -> state, Navigation.newUrl <| pageHash r
--- src/Client/View.fs
+++ src/Client/View.fs
@@ -13,17 +13,48 @@ open Fulma
 open Shared
 open Thoth.Json
 
+let navSource (model: AppModel) (dispatch: AppMsg -> unit) source =
+    let page =
+        source
+        |> Quests.Types.Page.SelectedSource
+        |> Page.Quests
+    Navbar.Item.a
+        [ Navbar.Item.Props
+            [ OnClick(fun _ ->
+                page
+                |> NavigateTo
+                |> dispatch) ]
+          (model.CurrentPage
+           |> Option.defaultValue Page.Home
+           |> (=) page
+           |> Navbar.Item.IsActive) ] [ str source ]
+
+let navBrand (model: AppModel) (dispatch: AppMsg -> unit) =
+    Navbar.navbar [ Navbar.Color IsWhite ]
+        [ Container.container []
+              [ Navbar.Brand.div []
+                    [ Navbar.Item.a
+                        [ Navbar.Item.CustomClass "brand-text"
+                          Navbar.Item.Props <| [ Href <| State.pageHash Page.Home ] ] [ str "SAFE Admin" ] ]
+
+                Navbar.menu []
+                    [ Navbar.Start.div []
+                          (match model.Quests.Sources with
+                           | Empty -> [ Navbar.Item.a [] [ str "Not requested" ] ]
+                           | Loading -> [ Navbar.Item.a [] [ str "Loading" ] ]
+                           | LoadError s -> [ Navbar.Item.a [] [ str <| sprintf "Error: %s" s ] ]
+                           | Body sources -> sources |> List.map (navSource model dispatch)) ] ] ]
 
 let view (model: AppModel) (dispatch: AppMsg -> unit) =
     div []
-        [ Container.container []
+        [ navBrand model dispatch
+          Container.container []
               [ Columns.columns []
-                    [ Column.column [ Column.Width(Screen.All, Column.Is3) ]
-                          [ sprintf "State: %A" model
-                            |> str ]
+                    [ Column.column [ Column.Width(Screen.All, Column.Is3) ] [ sprintf "State: %A" model |> str ]
 
                       Column.column [ Column.Width(Screen.All, Column.Is9) ]
                           (match model.CurrentPage with
+                           | None -> [ str "404" ]
                            | Some Home -> [ str "You are home!" ]
-                           | Some Recipes -> [ str "Recipes WIP!"]
+                           | Some Recipes -> [ str "Recipes WIP!" ]
                            | Some(Quests _) -> [ Quests.View.view model.Quests (QuestsMsg >> dispatch) ]) ] ] ]
