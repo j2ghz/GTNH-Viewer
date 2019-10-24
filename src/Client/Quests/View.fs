16d80200cbb14975202ba2c12e735143d3b24f78
J2ghz <j2.00ghz@gmail.com>
2019-10-24 12:35:51 +0200

    copy old questlineview

--- src/Client/Quests/View.fs
+++ src/Client/Quests/View.fs
@@ -44,6 +44,76 @@ let menu s (model: Types.State) urlMaker =
                              |> urlMaker
                              |> Href ] ] [ str qli.Name ])) ]
 
+let questLineInfo (ql: Shared.QuestLineInfo) =
+    Hero.hero []
+        [ Hero.body []
+              [ Container.container []
+                    [ Heading.h1 [] [ str ql.Name ]
+                      Heading.h5 [ Heading.IsSubtitle ] [ str ql.Description ] ] ] ]
+
+let prerequisiteBadge pr =
+    a
+        [ pr
+          |> sprintf "#/Quest/%i"
+          |> Href ]
+        [ Tag.tag []
+              [ pr
+                |> string
+                |> str ] ]
+
+let questCard (q: QuestLineQuest) =
+    Card.card []
+        [ Card.header []
+              [ Card.Header.title [] [ str q.Quest.Name ]
+                Card.Header.icon
+                    [ Props
+                        [ q.Id
+                          |> sprintf "#/Quest/%i"
+                          |> Href ] ] [ i [ ClassName "fa fa-hashtag" ] [] ] ]
+
+          Card.content []
+              [ Content.content []
+                    (match q.Quest.Prerequisites with
+                     | [] -> List.empty
+                     | prereqs -> [ (Tag.list [] (prereqs |> List.map prerequisiteBadge)) ]) ]
+
+          Card.content []
+              [ Content.content []
+                    [ pre [ Style [ WhiteSpaceOptions.PreWrap |> WhiteSpace ] ] [ str q.Quest.Description ] ] ] ]
+
+let questLineQuestGridItem (qlq: QuestLineQuest) =
+    let (x, y) = qlq.Location
+    let (sx, sy) = qlq.Size
+    a
+        [ qlq.Id
+          |> sprintf "#%i"
+          |> Href ]
+        [ div
+            [ Style
+                [ Height sx
+                  Width sy
+                  Position PositionOptions.Absolute
+                  Top x
+                  Left y
+                  Background "#000" ] ] [] ]
+
+let questLineView ql =
+    [ (ql.QuestLineInfo |> questLineInfo)
+      div
+          [ Style
+              [ Height
+                  (ql.Quests
+                   |> List.map (fun q -> fst q.Location + fst q.Size)
+                   |> List.max)
+                Width
+                    (ql.Quests
+                     |> List.map (fun q -> snd q.Location + snd q.Size)
+                     |> List.max)
+                Position PositionOptions.Relative
+                OverflowStyle OverflowOptions.Auto ]
+            Class "box" ] (ql.Quests |> List.map questLineQuestGridItem)
+      div [] (ql.Quests |> List.map questCard) ]
+
 let view (currentPage: Types.Page) urlMaker (model: Types.State) (dispatch: Types.Msg -> unit) =
     [ Columns.columns []
           [ Column.column [ Column.Width(Screen.All, Column.Is3) ]
@@ -59,10 +129,4 @@ let view (currentPage: Types.Page) urlMaker (model: Types.State) (dispatch: Type
                           | LoadError e ->
                               [ sprintf "Loading QuestLine failed:\n%s" e
                                 |> str ]
-                          | Body ql ->
-                              Hero.hero [ Hero.Color IsLight ]
-                                  [ Hero.body []
-                                        [ Container.container [ Container.IsFluid ]
-                                              [ Heading.h1 [] [ str ql.QuestLineInfo.Name ]
-                                                Heading.h2 [ Heading.IsSubtitle ] [ str ql.QuestLineInfo.Description ] ] ] ]
-                              :: (ql.Quests |> List.map (fun q -> p [] [ str q.Quest.Name ]))) ] ] ]
+                          | Body ql -> questLineView ql) ] ] ]
